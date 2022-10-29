b05191fc6de3821cb7e70994f436bebfd206242b
ive-m <ivesita.maria@gmail.com>
2022-10-29 15:40:43 -0400

    POST request to /urls it responds with a redirection to /urls/:id

--- express_server.js
+++ express_server.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 function generateRandomString() {
   let result = '';
   let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  for (let i = 6; i > 0; --i) 
-  result += chars[Math.floor(Math.random() * chars.length)];
+  for (let i = 6; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
   return result;
 }
 const urlDatabase = {
@@ -42,8 +42,16 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send(generateRandomString()); // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  const randomString = generateRandomString();
+  urlDatabase[randomString] = longURL;//add to database
+  res.send(res.redirect(`/urls/${randomString}`)); // redirect
+  
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL =  urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
