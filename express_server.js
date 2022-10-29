f5dd83d4c688f2c936cc3e3331eee3cc4b98217c
ive-m <ivesita.maria@gmail.com>
2022-10-28 22:05:54 -0400

    Add POST

--- express_server.js
+++ express_server.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+function generateRandomString() {
+  let result = '';
+  let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 6; i > 0; --i) 
+  result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -24,9 +32,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(generateRandomString()); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
