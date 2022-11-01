497d4281bd35e7a180a9c11a133bff8c7b05dc9e
ive-m <ivesita.maria@gmail.com>
2022-11-01 18:42:05 -0400

    Add login logout

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const users = [];
 
 function generateRandomString() {
   let result = '';
@@ -28,16 +31,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  console.log("Url Database", urlDatabase);
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id,username: req.cookies["username"], longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
@@ -49,6 +54,32 @@ app.post("/urls", (req, res) => {
   
 });
 
+
+app.post("/login", (req, res) => {
+
+  console.log(req.body);//require(body-parser)
+  console.log(req.body.username);
+  res.cookie("username", req.body.username);
+  const templateVars = {
+    username: req.cookies["username"],
+    // ... any other vars
+  };
+ //res.render("partials/_header", templateVars);
+ // res.render("urls", templateVars);
+  //res.render("urls/new", templateVars);
+  //res.render("urls/show", templateVars);
+ //res.render("urls_index", templateVars);
+  res.redirect(`/urls`); // redirect
+  
+});
+
+app.post('/sign-out', (req, res) => {
+  res.clearCookie('username'); // Tell the browser to delete this cookie.
+  res.redirect('/');
+});
+//app.get('/login', (req, res) => {
+//  if(req.cookies.user===user){const templateVAr={user = user} res.render()}
+//});
 app.post("/urls/:id/delete", (req, res) => {
 
   delete urlDatabase[req.params.id];
